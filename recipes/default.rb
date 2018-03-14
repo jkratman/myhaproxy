f8c85ac828eecfc30839c484ac6a7814c6c52563
jkratman <jkratman@hotmail.com>
2018-03-14 10:46:02 -0400

    Initial Commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+node.default['haproxy']['members'] = [{
+
+'hostname' => 'ec2-174-129-181-160.compute-1.amazonaws.com',
+'ipaddress' => '174.129.181.160',
+'port' => 80,
+'ssl_port' => 80
+}]
+
+include_recipe 'haproxy::manual'
